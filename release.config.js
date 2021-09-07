80d09e4c69f0a987bb2269cd2f290be02f18d951
cmulleradeo <cyril.muller@ext.adeo.com>
2021-09-07 10:46:13 +0200

    feat: add release note and changelog generator

---
+++ release.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        }
+      }
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        },
+        writerOpts: {
+          commitsSort: ["subject", "scope"]
+        }
+      }
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["docs/CHANGELOG.md"]
+      }
+    ]
+  ]
+}
