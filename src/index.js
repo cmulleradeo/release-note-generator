62498253496a7ffd10f0818d093fe42978ef24c1
cmulleradeo <cyril.muller@ext.adeo.com>
2021-09-07 09:58:31 +0200

    feat(init): rework counter and entry point

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-class Main {
-    counter
+import Counter from './Counter'
 
-    increment() {
-        this.counter++
+const main = () => {
+    const counters = []
+    for (let index = 0; index < 5; index++) {
+        counters.push(new Counter());
     }
 
-    decrement() {
-        this.counter--
-    }
+    counters[2].increment()
+    counters[3].increment()
+    counters[3].increment()
+    counters[3].increment()
 
-    get() {
-        return this.counter
+    for (let index = 0; index < counters.length; index++) {
+        console.log(`Counter ${i + 1} has a value of ${counters[i].get()}`)
     }
 }
-
-export default Main
