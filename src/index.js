c5852f4c86efa035ff22c13939af9107737bcd61
cmulleradeo <cyril.muller@ext.adeo.com>
2021-09-07 09:54:18 +0200

    feat(init): add main class

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Main {
+    counter
+
+    increment() {
+        this.counter++
+    }
+
+    decrement() {
+        this.counter--
+    }
+
+    get() {
+        return this.counter
+    }
+}
+
+export default Main
